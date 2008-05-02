0096e5c017993ed9f8fd8b46eaf577794c2f37a1
David A. Cuadrado <krawek@gmail.com>
2008-05-01 23:37:56 -0500

    Show the last update time for a repository
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -10,7 +10,7 @@ class Repository < ActiveRecord::Base
   has_many    :proposed_merge_requests, :foreign_key => 'source_repository_id', 
                 :class_name => 'MergeRequest', :order => "id desc", :dependent => :destroy
   has_many    :cloners, :dependent => :destroy
-  has_many    :events, :as => :target, :dependent => :destroy
+  has_many    :events, :as => :target, :order => "created_at desc", :dependent => :destroy
   
   validates_presence_of :user_id, :project_id, :name
   validates_format_of :name, :with => /^[a-z0-9_\-]+$/i,
